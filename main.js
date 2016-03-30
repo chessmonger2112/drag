5dff046fee4c597d8dd7555c7601a9ce330816fd
kyle smith <chessmonger2112@yahoo.com>
2016-03-30 16:32:54 -0700

    now works with all elements with .draggable

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-var mouseX;
+    var mouseX;
     var mouseY;
-    var jamX = 0;
-    var jamY = 0;
     var mouseDown = false;
+    var currJqEl = null;
+    var positions = [];
+    var jamLeft = (left,jEl) => jEl.css("left",left + "px");
+    var jamTop = (top, jEl) => jEl.css("top",top + "px");
+    var findPos = (direction, jEl) => jEl.position()[direction];
 
-    function jamLeft(left)
+    function addAttrsAndArrays(index, node)
     {
-      $("#jam").css("left",left + "px");
+      // var jamX = findPos("left",$(node));
+      // var jamY = findPos("top",$(node));
+      var leftMore = 300 * index
+      positions.push({jamX: leftMore, jamY: 0});
+      jamLeft(leftMore, $(node));
+      $(node).data("index", index);
     }
-    function jamTop(top)
-    {
-      $("#jam").css("top",top + "px");
-    }
+
+    $(".draggable").each(addAttrsAndArrays);
+
+
+//problem: second image starts off wrong by 50 px or so in the x direction
+
+
+
 
     $(document).mouseup(function(event) {
       mouseDown = false;
@@ -20,7 +32,6 @@ var mouseX;
     $(document).mousemove(function(event) {
       var newMouseX = event.clientX;
       var newMouseY = event.clientY;
-      console.log(`jamX: ${jamX}, jamY: ${jamY}`);
 
       if (mouseX === undefined)
       {
@@ -30,22 +41,26 @@ var mouseX;
       var deltaX = newMouseX - mouseX;
       var deltaY = newMouseY - mouseY;
 
-
-
       mouseX = newMouseX;
       mouseY = newMouseY;
 
       if (mouseDown)
       {
-        jamX += deltaX;
-        jamY += deltaY;
+        var currentIndex = currJqEl.data("index");
+        var currentPosition = positions[currentIndex];
+        currentPosition.jamX += deltaX;
+        currentPosition.jamY += deltaY;
+        var jamX = currentPosition.jamX;
+        var jamY = currentPosition.jamY;
+        console.log(`jamX: ${jamX}, jamY: ${jamY}`);
+        jamLeft(jamX, currJqEl);
+        jamTop(jamY, currJqEl);
       }
-        jamLeft(jamX);
-        jamTop(jamY);
     });
 
-    $("#jam").mousedown(function(event) {
+    $(".draggable").mousedown(function(event) { // lets it know mouse is currently in down state
       console.log("get down");
+      currJqEl = $(this);
       mouseX = event.clientX;
       mouseY = event.clientY;
       mouseDown = true;
